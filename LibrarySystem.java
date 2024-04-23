f1e539080bb6832d279098f57fe1a43429c708dc
whflwhfl <81403148+whflwhfl@users.noreply.github.com>
2024-04-23 16:54:23 +0900

    중간시험!
    good!

--- LibrarySystem.java
+++ LibrarySystem.java
@@ -1,10 +1,16 @@
 public class LibrarySystem {
 	// 변수 정의
+	private Book[] booklist;
+	private static int bookCount = 0;
+	private static final int NUM_BOOK = 10;
 	
 	// 생성자 정
+	public LibrarySystem() {
+		this.booklist = new Book[NUM_BOOK];
+	}
 
 	public void addBook(Book book) {
-		// 책 추가
+		booklist[bookCount++] = new LibraryBook(book);
 		// 도움을 위해 Cart.java의 insertBook(Book book)을 참조하세요
 	}
 	
@@ -33,35 +39,61 @@ public class LibrarySystem {
 	
 	public void borrowBook(String title) {
         // `for` LibrarySystem의 모든 Book {
+		for (int i = 0; i < bookCount; i++) {
             // `if` 책 제목이 발견되면 {
+			if(booklist[i].getTitle().equals(title)) {
                 // `if` 책이 있다면 {
+				if(booklist[i].getTitle() == title) {
+					boolean setAvailable = false;
+					System.out.println("책 있음");
+				}else
+					System.out.println("책 없음");
                     // setAvailable을 false로 설정하고 메시지를 출력합니다.
 				// } `else` 책이 없다면 {
 					// 메시지를 인쇄하다
 				// }
 				return; // 책 제거 후 종료 방법
 			// }
+			}
 		// }
+		}
 		// 찾을 수 없으면 메시지를 인쇄합니다.
 		System.out.println("# 오류: '" + title + "' 못 찾았습니다!");
 	}
 	
 	public void returnBook(String title) {
 	    // `for` LibrarySystem의 모든 Book {
+		for(int i = 0; i < bookCount; i++) {
             // `if` 책 제목이 발견되면 {
+			if(booklist[i].getTitle().equals(title)) {
                 // `if` 책이 없다면 {
+				if(booklist[i].getTitle() != title) {
+					boolean setAvailable = true;
+					System.out.println("책 없음");
+				}else
+					System.out.println("책 있음");
                     // setAvailable을 true로 설정하고 메시지를 출력합니다.
 				// } `else` 책이 있다면 {
 					// 메시지를 인쇄하다
 				// }
 				return; // 책 제거 후 종료 방법
 			// }
+			}
 		// }
+		}
 		// 찾을 수 없으면 메시지를 인쇄합니다.
 		System.out.println("# 오류: '" + title + "' 못 찾았습니다!");
 	}
 	
 	public void displayAllBooks() {
+		for(int i = 0; i < bookCount; i++) {
+			System.out.print(booklist[i].getIsbn() + "|");
+			System.out.print(booklist[i].getTitle() + "|");
+			System.out.print(booklist[i].getAuthor() + "|");
+			System.out.print(booklist[i].getYear() + "|");
+			System.out.print(booklist[i].isAvailable());
+			
+		}
 		// 책 목록 출력 (책이름, 저자, 출판년도, 대출가능여부 출력)
 
         /* 
